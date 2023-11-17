b0527ed184e951518d650e707ef09f587ba65d6e
zaini <104916711+mfchrzaini@users.noreply.github.com>
2023-11-17 13:24:08 +0700

    Update abstract-class.php

--- abstract-class.php
+++ abstract-class.php
@@ -1,21 +1,21 @@
 <?php
 // method dari abstract class bisa di akses oleh child
-abstract class coba {
-    public function sayHeyo() {
-        return "Say Heyo";
-    }
+// dalam contoh dibawah diberikan contoh case dimana kita tidak bisa melakukan intansiasi abstract class
+// jika class buah bukan abstrak maka method makan juga bukan abstrak dan bisa di akses saat di instansiasi sedangkan dalam kasus tersebut kita tidak tahu buah apa yang dimakan ketika kita menjalankan method makan
+// karena itu diperlukan penerapan abstract class dalam case tersebut
+abstract class buah {
+    abstract public function makan();
 }
 
 
-class anakcoba extends coba {
-     public function heyo() {
-        $str = "ini function dari mother {$this->sayHeyo()}";
-        return $str;
+class jeruk extends buah {
+     public function makan() {
+        echo "kupas jeruk lalu makan";
      }
 }
 
+$jeruk = new jeruk();
 
-$anak = new anakcoba;
+echo $jeruk->makan();
 
-echo $anak->heyo();
 ?>
